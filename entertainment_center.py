ea4e1369f14196423915019e1e06956e2307f986
jax11000 <jaxofalltrades11000@gmail.com>
2017-11-24 01:47:48 -0500

    conforms to pep8

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,46 +10,44 @@ avengers = media.Movie(
                        )
 
 hunger_games = media.Movie(
-    "Hunger Games", 
+    "Hunger Games",
     "Totalitarian society forces people to fight to the death",
     "https://upload.wikimedia.org/wikipedia/en/4/4b/Hunger_Games_Film_Poster.jpg",  # Noqa
     "https://www.youtube.com/watch?v=4S9a5V9ODuY")
 
 inglorious_bastards = media.Movie(
-    "Inglorious Bastards", 
+    "Inglorious Bastards",
     "Team of U.S. soldiers on the hunt for Nazis",
     "https://upload.wikimedia.org/wikipedia/en/c/c3/Inglourious_Basterds_poster.jpg",  # Noqa
     "https://www.youtube.com/watch?v=lb-9_4lGVmc")
 
 deadpool = media.Movie(
-    "Deadpool", 
+    "Deadpool",
     "Misfit hero fights evil",
     "https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",  # Noqa
     "https://www.youtube.com/watch?v=ONHBaC-pfsk")
 
 walking_tall = media.Movie(
-    "Walking Tall", 
+    "Walking Tall",
     "Vet returns from war to be town sheriff",
     "https://upload.wikimedia.org/wikipedia/en/e/e9/Walking_Tall_%282004_film%29.jpg",  # Noqa
     "https://www.youtube.com/watch?v=edR_niqKp1Q")
 
 iron_man = media.Movie(
-    "Iron Man", 
+    "Iron Man",
     "Billionaire makes super soldier suite",
     "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",  # Noqa
     "https://www.youtube.com/watch?v=8hYlB38asDY")
 
-
-
 movies = [
-    avengers, 
-    hunger_games, 
-    inglorious_bastards, 
-    deadpool, 
-    walking_tall, 
+    avengers,
+    hunger_games,
+    inglorious_bastards,
+    deadpool,
+    walking_tall,
     iron_man
     ]
 # use fresh_tomatoes.py to set up web page
 fresh_tomatoes.open_movies_page(movies)
-                          
+
 # end of file
